0d6a6eb77d3e068b205960277b63bb0c0a498dd4
chandev0908 <chandev0908@gmail.com>
2023-05-03 21:25:24 +0800

    fix layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,9 +7,9 @@ export default function Home() {
   const { animeList, loading, fetchAnimeRecent } = animeStore();
 
   return (
-    <main className="mx-auto">
+    <main className="m-auto w-full max-w-[1300px]">
       <Header></Header>
-      <div className="layout grid md:grid-cols-1fr-auto m-auto w-full justify-center">
+      <div className="layout grid md:grid-cols-1fr-auto grid-cols-none grid-flow-row m-auto w-full justify-center content-start justify-items-center">
         <RecentAnime></RecentAnime>
         <TopAnime></TopAnime>
       </div>
