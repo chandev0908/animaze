c9887a9f28637b44554afee93f3b77198ef7e890
chandev0908 <chandev0908@gmail.com>
2023-05-03 21:16:45 +0800

    classname change

--- components/RecentAnime.tsx
+++ components/RecentAnime.tsx
@@ -14,7 +14,7 @@ export default function RecentAnime() {
       <div className="h-fit grid grid-cols-2 grid-flow-row gap-6 text-white p-7 sm:grid-cols-3 md:grid-cols-4">
         {animeList.slice(0, 20).map((anime) => (
           <div
-            className="anime-card relative h-auto shadow-2xl text-center cursor-pointer max-w-[13rem] md:max-h-[22.5rem] md:max-w-[calc(16.66% - 14px)] transition-all duration-500 hover:bg-[#2D3148] rounded-lg hover:before:block hover:before:w-full hover:before:h-4/5 hover:before:bg-zinc-700 hover:before:opacity-70 hover:before:absolute hover:before:content-['Play'] before:m-auto"
+            className="anime-card relative h-auto shadow-2xl text-center cursor-pointer max-w-[13rem] md:max-h-[22.5rem] md:max-w-[calc(16.66% - 14px)] transition-all duration-500 hover:bg-[#2D3148] rounded-lg hover:before:block hover:before:w-full hover:before:h-4/5 hover:before:bg-zinc-700 hover:before:opacity-70 hover:before:absolute before:m-auto"
             key={anime.id}
           >
             <img
