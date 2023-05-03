812367d8b0ad26361d62c9d94a5c542d0e730867
chandev0908 <chandev0908@gmail.com>
2023-05-03 21:28:04 +0800

    changes types

--- store/types/topAnimeTypes.ts
+++ store/types/topAnimeTypes.ts
@@ -1,17 +1,39 @@
-interface TopAnimeResponse {
-  currentPage: number;
-  hasNextPage: boolean;
-  results: TopAnimeResults[];
-}
-interface TopAnimeResults {
-  id: string;
-  title: string;
-  image: string;
+export interface TopAnimeResults {
+  mal_id: number;
   url: string;
-  genres: string[];
+  images: {
+    jpg: {
+      image_url: string;
+      small_image_url: string;
+      large_image_url: string;
+    };
+    webp: {
+      image_url: string;
+      small_image_url: string;
+      large_image_url: string;
+    };
+  };
+  trailer: {
+    youtube_id: string;
+    url: string;
+    embed_url: string;
+    images: {
+      image_url: string;
+      small_image_url: string;
+      medium_image_url: string;
+      large_image_url: string;
+      maximum_image_url: string;
+    };
+  };
+  approved: boolean;
+  titles: {
+    type: string;
+    title: string;
+  }[];
+  title: string;
 }
 export interface TopAnimeTypes {
   topAnimeList: TopAnimeResults[];
-  loading: boolean;
+  topAnimeLoading: boolean;
   fetchTopAnime: () => Promise<void>;
 }
